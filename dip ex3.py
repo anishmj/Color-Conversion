6ed9692c27fbe8507d741dd300f074d3c01dc034
anishmj <mjanish19@gmail.com>
2023-03-28 09:37:18 +0530

    asd

--- dip ex3.py
+++ dip ex3.py
@@ -1,31 +1,9 @@
-# Color Conversion
-## AIM
-To perform the color conversion between RGB, BGR, HSV, and YCbCr color models.
+#!/usr/bin/env python
+# coding: utf-8
 
-## Software Required:
-Anaconda - Python 3.7
-## Algorithm:
-### Step1:
-Import cv2 library and upload the image or capture an image.
-### Step2:
-Read the saved image using cv2.imread("filename.jpg").
+# In[1]:
 
-### Step3:
-Convert the image into the given color transformation using cv2.cvtColor(image, cv2.BGR2YCrCb) and similarly for other color formats.
 
-### Step4:
-Split and merge the image using cv2.split(hsv) and cv2.merge([h,s,v])
-
-### Step5: 
-Output the image using cv2.imshow("OUTPUT", image)
-
-## Program:
-~~~
- Developed By: ANISH MJ
- Register Number: 212221230005
-~~~
-~~~
-# i) Convert BGR and RGB to HSV and GRAY
 # BGR TO HSV
 
 import cv2
@@ -35,6 +13,10 @@ b_h=cv2.cvtColor(image,cv2.COLOR_BGR2HSV)
 cv2.imshow('BGR_HSV',b_h)
 cv2.waitKey(0)
 cv2.destroyAllWindows
+
+
+# In[2]:
+
 
 # BGR TO GRAY
 
@@ -46,6 +28,10 @@ cv2.imshow('BGR_GRAY',b_g)
 cv2.waitKey(0)
 cv2.destroyAllWindows
 
+
+# In[3]:
+
+
 # RGB TO HSV
 
 import cv2
@@ -55,6 +41,10 @@ r_h=cv2.cvtColor(image,cv2.COLOR_RGB2HSV)
 cv2.imshow('RGB_HSV',r_h)
 cv2.waitKey(0)
 cv2.destroyAllWindows
+
+
+# In[ ]:
+
 
 # RGB TO GRAY
 
@@ -67,9 +57,8 @@ cv2.waitKey(0)
 cv2.destroyAllWindows
 
 
+# In[ ]:
 
-
-# ii)Convert HSV to RGB and BGR
 
 # HSV TO RGB
 
@@ -80,6 +69,10 @@ h_r=cv2.cvtColor(ori,cv2.COLOR_HSV2RGB)
 cv2.imshow('HSV_RGB',h_r)
 cv2.waitKey(0)
 cv2.destroyAllWindows
+
+
+# In[ ]:
+
 
 # HSV TO BGR
 
@@ -92,9 +85,8 @@ cv2.waitKey(0)
 cv2.destroyAllWindows
 
 
+# In[1]:
 
-
-# iii)Convert RGB and BGR to YCrCb
 
 # RGB TO YCrCb
 
@@ -105,19 +97,22 @@ cv2.imshow('RGB_YCRCB',YCrCb_image)
 cv2.waitKey(0)
 cv2.destroyAllWindows
 
+
+# In[1]:
+
+
 # BGR TO YCrCb
 
 import cv2
-image1=cv2.imread('ship.jpg')
+image1=cv2.imread('hsv2bgr.png')
 YCrCb_image = cv2.cvtColor(image1, cv2.COLOR_BGR2YCrCb)
 cv2.imshow('BGR_YCRCB',YCrCb_image)
 cv2.waitKey(0)
 cv2.destroyAllWindows
 
 
+# In[1]:
 
-
-# iv)Split and Merge RGB Image
 
 import cv2
 img = cv2.imread("ship.jpg")
@@ -133,44 +128,27 @@ cv2.waitKey(0)
 cv2.destroyAllWindows()
 
 
-# v) Split and merge HSV Image
+# In[2]:
+
+
 
 import cv2
 img = cv2.imread("ship.jpg")
 img1= cv2.resize(img, (270,180))
-cv2
-b,g,r = cv2.split(img1)
-cv2.imshow("RED MODEL", r)
-cv2.imshow("GREEN MODEL", g)
-cv2.imshow("BLUE MODEL ", b)
-merger = cv2.merge([b,g,r])
+hsv = cv2.cvtColor(img1 , cv2.COLOR_BGR2HSV)
+cv2.imshow("INITIAL_HSV ", hsv)
+h,s,v = cv2.split(hsv)
+cv2.imshow("RED MODEL", h)
+cv2.imshow("GREEN MODEL", s)
+cv2.imshow("BLUE MODEL ", v)
+merger = cv2.merge([h,s,v])
 cv2.imshow("MERGED IMAGE", merger )
 cv2.waitKey(0)
 cv2.destroyAllWindows()
 
-~~~
-## Output:
-### i) BGR and RGB to HSV and GRAY
-![am](ori.png)
 
-![am](bgr2hsv.png)
-![am](bgr2gray.png)
-![am](rgb2hsv.png)
-![am](rgb2gray.png)
-
-### ii) HSV to RGB and BGR
-![am](hsv2rgb.png)
-![am](hsv2bgr.png)
-
-### iii) RGB and BGR to YCrCb
-![am](rgb2ycrcb.png)
-![an](bgr2ycrcb.png)
-### iv) Split and merge RGB Image
-![am](merge.png)
-
-### v) Split and merge HSV Image
-![am](splitndmerge.png)
+# In[ ]:
 
 
-## Result:
-Thus the color conversion was performed between RGB, HSV and YCbCr color models.
+
+
